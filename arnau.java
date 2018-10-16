fc11c64321f5f761bcda3d2de2b0e35ed286aa15
arnau <arnau.arimon@gmail.com>
2018-10-16 17:05:44 +0200

    Moments

--- arnau.java
+++
@@ -1,9 +0,0 @@
-package empaquetar;
-	public class arnau{
-		public static void main(String[] args){
-		
-			System.out.println("Hola Arnau!");			
-				
-		}	
-	
-	}
---
+++ pepelu/HolaPepelu.java
@@ -0,0 +1,6 @@
+package pepelu;
+	public class HolaPepelu{
+		public static void main(String [] args){
+			System.out.println("Hola Arnau");
+		}
+	}
